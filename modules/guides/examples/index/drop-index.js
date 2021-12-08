8822d67f7be462ddef0c8c3ffc739872d7c4d122
Simon Dew <Simon.Dew@couchbase.com>
2021-12-08 00:50:27 +0000

    Add new SDK examples for dropping indexes

---
+++ modules/guides/examples/index/drop-index.js
@@ -0,0 +1,9 @@
+// tag::manager[]
+    // TODO: Node.js example
+// end::manager[]
+// tag::primary[]
+    // TODO: Node.js example
+// end::primary[]
+// tag::secondary[]
+    // TODO: Node.js example
+// end::secondary[]
---
+++ modules/guides/examples/index/drop-index.py
@@ -0,0 +1,9 @@
+# tag::manager[]
+    # TODO: Python example
+# end::manager[]
+# tag::primary[]
+    # TODO: Python example
+# end::primary[]
+# tag::secondary[]
+    # TODO: Python example
+# end::secondary[]
