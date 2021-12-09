eeb97009d0061c18bd84769d2462d70b5e301584
Simon Dew <Simon.Dew@couchbase.com>
2021-12-09 00:34:42 +0000

    Expand SDK instructions

--- modules/guides/examples/index/create-index.py
+++ modules/guides/examples/index/create-index.py
@@ -1,6 +1,6 @@
 # tag::primary[]
 # TODO: Python example
-# end::primary[]
+mgr.create_primary_index('bucket_name', CreatePrimaryQueryIndexOptions(ignore_if_exists=True))
 # tag::secondary[]
-# TODO: Python example
+mgr.create_index('bucket_name', 'idx_foo', fields=['foo'])
 # end::secondary[]
