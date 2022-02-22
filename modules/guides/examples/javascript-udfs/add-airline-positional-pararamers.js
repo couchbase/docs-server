ff9f3dc677946eea25a09b3938f171a922943e0d
Ray Offiah <ray.offiah@couchbase.com>
2022-02-22 13:43:35 +0000

    Work in progress

---
+++ modules/guides/examples/javascript-udfs/add-airline-positional-pararamers.js
@@ -0,0 +1,3 @@
+function addAirlineWithPositionalParameters(name, callsign, country) {
+    var q = N1QL("insert into `travel-sample`.`inventory`.`airport` values(\"1600\", {\"id\": $1, \"type\": $2, \"name\": $3, \"ca1\": $4, \"icao\": $5, \"callsign\": $6, \"country\": $7})", [1600, "airline", name, "Q5", "MLA", callsign, country])
+}
