95bd7805a774c683b6a6d43420066c2bbe6b626a
Ray Offiah <ray.offiah@couchbase.com>
2022-02-28 14:58:45 +0000

    Added error-handling page and a file for further reading

---
+++ modules/guides/examples/javascript-udfs/add-airline-with-check.js
@@ -0,0 +1,25 @@
+function addAirlineWithCheck(id, name, callsign, country) {
+
+    const full_id = "airline_" + id   
+    const d = new Date().toJSON();    
+
+    try {
+        
+        var q = insert into `travel-sample`.`inventory`.`airline` values(
+            $full_id,
+            {"id": $id,
+                "type": "airline",
+                "name": $name,
+                "ca1": "Q5",
+                "icao": "MLA",
+                "callsign": $callsign,
+                "country": $country,
+                "opdate": $d}); 
+        
+        return "success"
+    }
+    catch (error) {
+        return "failure"
+    }
+
+}
