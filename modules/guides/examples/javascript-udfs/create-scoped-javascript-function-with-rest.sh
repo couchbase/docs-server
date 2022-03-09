2b8c9b4975814f7149a8ab80004ec21482c1e0a1
Ray Offiah <ray.offiah@couchbase.com>
2022-03-09 17:52:04 +0000

    Work in progress

--- modules/guides/examples/javascript-udfs/create-scoped-javascript-function-with-rest.sh
+++ modules/guides/examples/javascript-udfs/create-scoped-javascript-function-with-rest.sh
@@ -1,6 +1,6 @@
-curl -v POST http://localhost:8093/evaluator/v1/libraries/my-library  \   # <.>
- -u Administrator:password \    # <.>
- -d 'function getBusinessDays(startDate, endDate) {    # <.>
+curl -v -X POST  'http://node1-neo-testing-ubuntu18.vagrants:8093/evaluator/v1/libraries/my-library?bucket=travel-sample&scope=inventory' \  #<.>
+ -u Administrator:password \
+ -d 'function getBusinessDays(startDate, endDate) {
           let count = 0;
           const curDate = new Date(new Date(startDate).getTime());
           while (curDate <= new Date(endDate)) {
