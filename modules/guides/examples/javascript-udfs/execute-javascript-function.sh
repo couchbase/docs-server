7028f8b458ddd2079a0db98b0adde84d6a48f775
Ray Offiah <ray.offiah@couchbase.com>
2022-03-12 05:51:54 +0000

    Revert "Revert "Work in progress""
    This reverts commit 46961338f3e3621d981f204e09b6ff7cf99f4737.

--- modules/guides/examples/javascript-udfs/execute-javascript-function.sh
+++ modules/guides/examples/javascript-udfs/execute-javascript-function.sh
@@ -1,3 +1,3 @@
-curl -v http://node1-neo-testing-ubuntu18.vagrants:8093/query/service \
+curl -v http://localhost:8093/query/service \
   -u Administrator:password \
   -d 'statement=EXECUTE FUNCTION GetBusinessDays("02/14/2022", "04/16/2022")'
