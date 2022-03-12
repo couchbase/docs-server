46961338f3e3621d981f204e09b6ff7cf99f4737
Ray Offiah <ray.offiah@couchbase.com>
2022-03-12 05:45:47 +0000

    Revert "Work in progress"
    This reverts commit 123427266272533ddb88d73d7b6ca0d9955fc156.

--- modules/guides/examples/javascript-udfs/create-scoped-javascript-function-with-rest.sh
+++ modules/guides/examples/javascript-udfs/create-scoped-javascript-function-with-rest.sh
@@ -1,4 +1,4 @@
-curl -v -X POST  'http://localhost:8093/evaluator/v1/libraries/my-library?bucket=travel-sample&scope=inventory' \  #<.>
+curl -v -X POST  'http://node1-neo-testing-ubuntu18.vagrants:8093/evaluator/v1/libraries/my-library?bucket=travel-sample&scope=inventory' \  #<.>
  -u Administrator:password \
  -d 'function getBusinessDays(startDate, endDate) {
           let count = 0;
