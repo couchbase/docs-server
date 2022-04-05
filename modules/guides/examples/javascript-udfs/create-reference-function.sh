73d90330538c8f4aa95966e7c3d54b5cabb260e3
Ray Offiah <ray.offiah@couchbase.com>
2022-04-05 17:52:02 +0100

    Renamed 'Reference function' to 'N1QL User-Defined Function'

--- modules/guides/examples/javascript-udfs/create-reference-function.sh
+++
@@ -1,3 +0,0 @@
-curl -v http://localhost:8093/query/service \
-  -u Administrator:password \
-  -d 'statement=CREATE FUNCTION GetBusinessDays(startDate, endDate) LANGUAGE JAVASCRIPT AS "getBusinessDays" AT "my-library";'
--- modules/guides/examples/javascript-udfs/drop-n1ql-reference-function.sh
+++
@@ -1,3 +0,0 @@
-curl -v http://localhost:8093/query/service \
-  -u Administrator:password \
-  -d 'statement=DROP FUNCTION GetBusinessDays;'
