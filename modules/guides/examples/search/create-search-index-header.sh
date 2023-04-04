05405383530f445678a914c21972a1f64c84f843
Sarah Welton <sarah.welton@couchbase.com>
2023-04-04 16:11:33 -0400

    [AV-43732] Removing content that has been transferred to docs-devex

--- modules/guides/examples/search/create-search-index-header.sh
+++
@@ -1,3 +0,0 @@
-curl -s -XPUT -H "Content-Type: application/json" \
--u ${CB_USERNAME}:${CB_PASSWORD} http://${CB_HOSTNAME}:8094/api/index/landmark-content-index 
--d \
--- modules/guides/examples/search/run-search-header.sh
+++
@@ -1,2 +0,0 @@
-curl -s -XPUT -H "Content-Type: application/json" \
--u ${CB_USERNAME}:${CB_PASSWORD} http://${CB_HOSTNAME}:8094/api/index/${INDEX-NAME}/query -d \
