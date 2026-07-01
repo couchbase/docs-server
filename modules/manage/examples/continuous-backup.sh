0e8a30554876d2680d533d826bbdc5e033e84c9e
Gary Gray <gary.gray@couchbase.com>
2026-07-01 14:40:26 -0400

    Wrapped up draft of PITR page.

---
+++ modules/manage/examples/continuous-backup.sh
@@ -0,0 +1,15 @@
+# Get information about the continuous backup location
+# tag::get-backup-info[]
+cbcontbk info -l /shared/continuous-backup/
+# end::get-backup-info[]
+
+# Restore the bucket
+# tag::restore-bucket[]
+cbcontbk restore --target 2026-06-08T19:54:10Z \
+         -u $COUCHBASE_USERNAME -p $COUCHBASE_PASSWORD \
+         --tmp-dir /scratch \
+         -l /shared/continuous-backup/ \
+         -a /shared/backups \
+         -r 1c48c43c-45b8-4155-8352-7d21523b2603 \
+         -c couchbase://node3.example.com
+# end::restore-bucket[]
